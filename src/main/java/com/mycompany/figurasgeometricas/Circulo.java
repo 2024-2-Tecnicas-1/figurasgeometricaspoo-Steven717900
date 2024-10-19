5cde00bbadf53314387d44cfedd2f498f0a341de
Steven717900 <pospinachico@gmail.com>
2024-10-19 18:01:40 -0500

    Update Circulo.java
    Se sube el codigo para calcula P y A

--- src/main/java/com/mycompany/figurasgeometricas/Circulo.java
+++ src/main/java/com/mycompany/figurasgeometricas/Circulo.java
@@ -8,6 +8,31 @@ package com.mycompany.figurasgeometricas;
  *
  * @author Steven xd
  */
-public class Circulo {
-    
+public class Circulo extends FiguraGeometrica {
+
+    private double radio;
+
+    public Circulo(String nombre, String color) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+
+    @Override
+    public double obtenerArea() {
+        return 2 * 3.1456 * radio * radio;
+    }
+
+    @Override
+    public double obtenerPerimetro() {
+        return 2 * 3.1456 * radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+
+    }
 }
