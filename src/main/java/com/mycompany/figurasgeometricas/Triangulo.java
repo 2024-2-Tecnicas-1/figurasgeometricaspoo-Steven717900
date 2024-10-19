0de554cc7bd402f5bf4de79ea0adcc2f173922de
Steven717900 <pospinachico@gmail.com>
2024-10-19 18:13:50 -0500

    Se sube codigo para calcular P y A de triangulo

--- src/main/java/com/mycompany/figurasgeometricas/Triangulo.java
+++ src/main/java/com/mycompany/figurasgeometricas/Triangulo.java
@@ -8,6 +8,34 @@ package com.mycompany.figurasgeometricas;
  *
  * @author Steven xd
  */
-public class Triangulo {
-    
+public class Triangulo extends FiguraGeometrica {
+
+    private double base;
+    private double altura;
+
+    public Triangulo(String nombre, String color) {
+        super(nombre, color);
+        this.base = base;
+        this.altura = altura;
+    }
+    @Override
+    public double obtenerArea() {
+        return 0.5 * base * altura;
+    }
+    @Override
+    public double obtenerPerimetro() {
+        return 3 * base;
+    }
+    public double getBase() {
+        return base;
+    }
+    public void setBase(double base) {
+        this.base = base;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
 }
