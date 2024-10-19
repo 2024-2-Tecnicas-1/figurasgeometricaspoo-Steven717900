c8b0bf1776c9cdb4f32c0a9e6c425217dff06bd3
Steven <Pospinachico@gmail.com>
2024-10-19 18:00:15 -0500

    Se agrega la clase sup y las de herencia

---
+++ src/main/java/com/mycompany/figurasgeometricas/FiguraGeometrica.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.figurasgeometricas;
+
+/**
+ *
+ * @author Steven xd
+ */
+public class FiguraGeometrica {
+
+    private String nombre;
+    private String color;
+
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public double obtenerArea() {
+        return 0;
+    }
+
+    public double obtenerPerimetro() {
+        return 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
