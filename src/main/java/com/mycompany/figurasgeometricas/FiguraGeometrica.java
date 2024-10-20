75b54be3d72181ddd018b1180deefd92cc8e69ea
Steven717900 <pospinachico@gmail.com>
2024-10-19 19:31:31 -0500

    se sube la complejidad

--- src/main/java/com/mycompany/figurasgeometricas/FiguraGeometrica.java
+++ src/main/java/com/mycompany/figurasgeometricas/FiguraGeometrica.java
@@ -13,27 +13,33 @@ public class FiguraGeometrica {
     private String nombre;
     private String color;
 
+    // Complejidad: O(1)
     public FiguraGeometrica(String nombre, String color) {
         this.nombre = nombre;
         this.color = color;
     }
 
+    // Complejidad: O(1)
     public double obtenerArea() {
         return 0;
     }
 
+    // Complejidad: O(1)
     public double obtenerPerimetro() {
         return 0;
     }
 
+    // Complejidad: O(1)
     public String getNombre() {
         return nombre;
     }
 
+    // Complejidad: O(1)
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    // Complejidad: O(1)
     public void setColor(String color) {
         this.color = color;
     }
