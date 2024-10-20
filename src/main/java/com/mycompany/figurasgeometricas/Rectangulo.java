bb00e888fcdc1e4e69b6f89a5a42a7d2190b9dcc
Steven717900 <pospinachico@gmail.com>
2024-10-19 19:32:51 -0500

    se sube la complejidad

--- src/main/java/com/mycompany/figurasgeometricas/Rectangulo.java
+++ src/main/java/com/mycompany/figurasgeometricas/Rectangulo.java
@@ -4,35 +4,44 @@
  */
 package com.mycompany.figurasgeometricas;
 
-/**
- *
- * @author Steven xd
- */
 public class Rectangulo extends FiguraGeometrica {
-    private double ladoAncho;
-    private double ladoLongitud;
+    private double ladoAncho; 
+    private double ladoLongitud; 
+
     public Rectangulo(String nombre, String color, double ladoAncho, double ladoLongitud) {
-        super(nombre, color);
+        super(nombre, color); 
         this.ladoAncho = ladoAncho;
         this.ladoLongitud = ladoLongitud;
     }
+
+    // Complejidad: O(1)
     @Override
     public double obtenerArea() {
         return ladoAncho * ladoLongitud; 
     }
+
+    // Complejidad: O(1)
     @Override
     public double obtenerPerimetro() {
         return 2 * (ladoAncho + ladoLongitud);
     }
+
+    // Complejidad: O(1)
     public double getLadoAncho() {
         return ladoAncho;
     }
+
+    // Complejidad: O(1)
     public void setLadoAncho(double ladoAncho) {
         this.ladoAncho = ladoAncho;
     }
+
+    // Complejidad: O(1)
     public double getLadoLongitud() {
         return ladoLongitud;
     }
+
+    // Complejidad: O(1)
     public void setLadoLongitud(double ladoLongitud) {
         this.ladoLongitud = ladoLongitud;
     }
